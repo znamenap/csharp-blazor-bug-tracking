16a609c6454461d5dffc53b7371ea528fc0def83
Alex Wolf <alexrudiwolf@gmail.com>
2020-04-19 21:23:29 -0400

    solution

--- BugTrackerUI/Bug.cs
+++ BugTrackerUI/Bug.cs
@@ -9,11 +9,16 @@ namespace BugTrackerUI
     public class Bug
     {
         public int Id { get; set; }
-
+        
+        [Required]
         public string Title { get; set; }
 
+        [Required]
+        [MinLength(10)]
         public string Description { get; set; }
 
+        [Required]
+        [Range(1, 5)]
         public int Priority { get; set; }
     }
 }
