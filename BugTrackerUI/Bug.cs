ab89e337a682f01991a60d5ada270ff771c0cc26
Pavel Znamenáček <pavel.znamenacek@gmail.com>
2020-11-24 21:52:10 +0100

    Add Data Validation to the Form

--- BugTrackerUI/Bug.cs
+++ BugTrackerUI/Bug.cs
@@ -10,10 +10,15 @@ namespace BugTrackerUI
     {
         public int Id { get; set; }
 
+        [Required]
         public string Title { get; set; }
 
+        [Required]
+        [MinLength(10)]
         public string Description { get; set; }
 
+        [Required]
+        [Range(1, 5)]
         public int Priority { get; set; }
     }
 }
